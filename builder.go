ff8ce0858e20797425f71073e231dbf025932c9e
Kimiblock Moe <kimiblock@icloud.com>
2026-04-03 23:13:29 +0800

    introduce prepDepRepo to prepare git repo

--- builder.go
+++ builder.go
@@ -1,14 +1,73 @@
 package main
 
 import (
-	"os/exec"
 	"log"
-	"syscall"
-	"path/filepath"
-	"os"
-	"strconv"
 	"math/rand"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"syscall"
 )
+
+// Prepare a Git repository for dependency building, returns the build directory
+func prepDepRepo(debug *log.Logger, warn *log.Logger, pkgname string, url string, prefix string) (string) {
+	errChan := make(chan error, 16)
+	go func () {
+		for sig := range errChan {
+			if sig != nil {
+				warn.Fatalln("Could not prepare git repository:", sig)
+			}
+		}
+	} ()
+	var path string = filepath.Join(xdgDir.cacheDir, "stashpak/git", pkgname)
+
+	debug.Println("Preparing a build directory...")
+	cmdline := []string{
+		"remote",
+		"get-url",
+		"origin",
+	}
+	cmd := exec.Command("git", cmdline...)
+	cmd.Dir = path
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Pdeathsig:		syscall.SIGTERM,
+	}
+	out, err := cmd.Output()
+	if err != nil {
+		debug.Println("Could not get origin URL of repository:", err)
+		err = getRemoteGit(path, url)
+		if err != nil {
+			errChan <- err
+			warn.Println(err)
+		}
+	} else if strings.TrimSpace(string(out)) != url {
+		warn.Println("Repository origin mismatch:", string(out), "!=", url)
+		err := getRemoteGit(path, url)
+		if err != nil {
+			warn.Println(err)
+			errChan <- err
+		}
+	}
+	cmdline = []string{
+		"pull",
+	}
+	cmd = exec.Command("git", cmdline...)
+	cmd.Dir = path
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Pdeathsig:		syscall.SIGTERM,
+	}
+	err = cmd.Run()
+	if err != nil {
+		warn.Println("Could not update repository:", err)
+		errChan <- err
+	}
+	close(errChan)
+	return path
+
+
+}
 
 // Builds a package from git repository using chroot, returns the path to build directory and optionally a slice of errors
 func buildPkg(debug *log.Logger, warn *log.Logger, pkgname string, url string, prefix string) (string, []error) {
@@ -32,7 +91,7 @@ func buildPkg(debug *log.Logger, warn *log.Logger, pkgname string, url string, p
 			errChan <- err
 			warn.Println(err)
 		}
-	} else if string(out) != url {
+	} else if strings.TrimSpace(string(out)) != url {
 		warn.Println("Repository mismatch, downloading from source")
 		err := getRemoteGit(buildPath, url)
 		if err != nil {
