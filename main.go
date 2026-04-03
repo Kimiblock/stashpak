4394224d9cce517909e50180181a43b2d1d48a0a
Kimiblock Moe <kimiblock@icloud.com>
2026-04-03 22:37:42 +0800

    separate vars

--- main.go
+++ main.go
@@ -20,61 +20,6 @@ import (
 	alpm "github.com/Jguer/go-alpm/v2"
 )
 
-var (
-	conf		envConf
-	xdgDir		xdg
-	elevate		= make(chan elevateRequest, 2)
-)
-
-const (
-	repoUrl		string = "https://github.com/Kraftland/portable-arch.git"
-)
-
-// Client must initialize success / error channel!
-type elevateRequest struct {
-	cmdline		[]string
-	timeout		time.Duration
-	wd		string
-	err		chan error
-}
-
-type xdg struct {
-	runtimeDir		string
-	confDir			string
-	cacheDir		string
-	dataDir			string
-	home			string
-}
-
-type pkgConf struct {
-	// Arrays of tables containing dependencies not from core/extra
-	Depends		[]DependsSection
-	Metadata	confMeta
-}
-
-type confMeta struct {
-	// The build prefix. Defaults to "extra-x86_64-build".
-	BuildPrefix	string
-	// GitHub user name of Maintainer
-	Maintainer	string
-	Type		string
-	Repo		string
-}
-
-type DependsSection struct {
-	Pkgname		string
-	// Source can be either a string of git URL (type git), or repo name (type repo) to download from locally defined repositories.
-	SourceType	string
-	Source		string
-	// The build prefix for type git. Defaults to "extra-x86_64-build".
-	BuildPrefix	string
-	Install		bool
-}
-
-type envConf struct {
-	elevateProgram		string
-}
-
 func decodeConf (path string, warn *log.Logger) (pkgConf, error) {
 	var res pkgConf
 	res.Metadata.Type = "build"
