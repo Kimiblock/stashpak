281bd37b05980183b67d580f135d29655fb47ad0
Kimiblock Moe <kimiblock@icloud.com>
2026-04-04 15:11:33 +0800

    cleanDir: delete package files on error

--- git.go
+++ git.go
@@ -19,6 +19,7 @@ func cleanDir(path string, debug *log.Logger, warn *log.Logger) {
 	err := cmd.Run()
 	if err != nil {
 		warn.Println("Could not reset repository:", err)
+		delPkgs(path, debug, warn)
 	}
 	cmdline = []string{
 		"clean",
@@ -32,5 +33,6 @@ func cleanDir(path string, debug *log.Logger, warn *log.Logger) {
 	err = cmd.Run()
 	if err != nil {
 		warn.Println("Could not clean repository:", err)
+		delPkgs(path, debug, warn)
 	}
 }
